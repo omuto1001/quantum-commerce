18535294f275d3908b04603ee0ce012d7c6cf73e
Matovu James <omulangilamato@gmail.com>
2026-07-21 10:52:00 +0300

    Add soft gradient background to auth pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gradient-to-br from-indigo-100 via-purple-50 to-blue-100">
             <div>
                 <a href="/" class="flex flex-col items-center">
     <x-application-logo class="w-24 h-16 text-indigo-500" />
